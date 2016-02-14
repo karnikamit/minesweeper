ccbd41352f25306f1b74d6a1744e93820b952d7a
Amit Karnik <amit.karnik@edgenetworks.in>
2016-02-14 07:32:20 +0530

    Check field clearance

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -8,6 +8,7 @@ class Grid:
         self.n = n
         self.m = m
         self.valid_values = ['0', '1', '2', 'm']
+        self.counter = 0
         self.field = self.build_grid()
 
     def build_grid(self):
@@ -17,6 +18,7 @@ class Grid:
             for j in xrange(self.m):
                 c = Cell(self.valid_values[random.randint(0, 3)], 'closed', 'x')
                 row.append(c)
+                self.counter += 1
             f.append(row)
         return f
 
@@ -31,13 +33,16 @@ class Grid:
     def open_cell(self, f1, f2, flag=None):
         value = None
         if self.field[f1][f2].status == 'closed':
+            self.field[f1][f2].status = 'open'
             if not flag:
                 self.field[f1][f2].view = 'O'
-                self.field[f1][f2].status = 'open'
+                value = self.field[f1][f2].value
             else:
                 self.field[f1][f2].view = 'f'
-            value = self.field[f1][f2].value
         return value
+
+    def check(self):
+        return self.counter
 
 
 class Cell:
@@ -51,8 +56,11 @@ class Cell:
 
 
 def main():
-    grid = Grid(2, 2)
+    ip = raw_input('Enter the size of the grid like 2*2: ')
+    rows, columns = int(ip[0]), int(ip[2])
+    grid = Grid(rows, columns)
     death = False
+    c = 0
     while not death:
         print grid.show()
         options = raw_input('Enter option: ')
@@ -66,5 +74,11 @@ def main():
         if v_value == 'm':
             print 'Oops, you stepped on a mine! Game over'
             break
+        c += 1
+        if c == grid.check():
+            print 'You cleared the minefield. Game over!'
+            break
 
-main()
+
+if __name__ == '__main__':
+    main()
